f0010c8a0d53949f396b5765d036b6e09150d9af
DMcDonald57 <d.mcdonald579@gmail.com>
2022-12-08 21:32:18 -0700

    created login and game view.  cleaned up code and set up routes

---
+++ src/views/GamePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Lets Play!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        Name : "GamePage"
+    }
+</script>
+
+<style scoped>
+
+</style>
